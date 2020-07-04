e418347557e6855918dd4c7e9ed3e6cab1185004
tariqul.isha@gmail.com <tariqul.isha@gmail.com>
2020-07-04 20:30:28 +0600

    feat(leetcode): solve 3sum closest

--- leetcode/16_3Sum_closest.cpp
+++ leetcode/16_3Sum_closest.cpp
@@ -3,9 +3,12 @@ using namespace std;
 
 class Solution {
 public:
-    int diff = INT_MAX;
+    int getClosestValue(int target, int val1, int val2) {
+        return abs(target - val1) < abs(target - val2) ? val1 : val2;
+    }
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
+        int result = INT_MAX;
         
         for(int i=0; i<nums.size() - 2 && diff != 0; i++) {
             int low = i+1, high = nums.size() - 1;
@@ -13,9 +16,7 @@ public:
             while(low < high) {
                 int sum = nums[i]+nums[low]+nums[high];
                 
-                if(abs(target - sum) < abs(diff)) {
-                    diff = target - sum;
-                }
+                result = getClosestValue(target, result, sum);
                 
                 if (sum < target) {
                     low++;
