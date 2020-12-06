131b70da8d78c10a025355c974d25c7dfa950fc2
tariqul.isha@gmail.com <tariqul.isha@gmail.com>
2020-12-07 00:58:32 +0600

    Leetcode revision
    closes #81

--- leetcode/1048_string_chain.cpp
+++ leetcode/1048_string_chain.cpp
@@ -1,48 +1,56 @@
-class Solution {
+#include "array.cpp"
+
+bool compare(const string &a, const string &b)
+{
+    return a.size() < b.size();
+}
+
+class Solution
+{
 public:
     unordered_map<string, vector<string>> successorMap;
-    unordered_map<string, int> resultMap;
-    void generateSuccessors(vector<string>& words) {
+    void generateSuccessors(vector<string> &words)
+    {
         unordered_set<string> wordSet(words.begin(), words.end());
         unordered_map<string, unordered_set<string>> seen;
-        
-        for(string str: words) {
-            for(int i=0; i<str.size(); i++) {
+
+        for (string str : words)
+        {
+            for (int i = 0; i < str.size(); i++)
+            {
                 string prefix = i == 0 ? "" : str.substr(0, i);
-                string suffix = i == str.size() - 1 ? "" : str.substr(i+1, str.size() - i - 1);
+                string suffix = i == str.size() - 1 ? "" : str.substr(i + 1, str.size() - i - 1);
                 string pre = prefix + suffix;
-                
-                if (wordSet.find(pre) != wordSet.end() 
-                    && (seen.find(pre) == seen.end() || seen[pre].find(str) == seen[pre].end())) {
+
+                if (wordSet.find(pre) != wordSet.end() && (seen.find(pre) == seen.end() || seen[pre].find(str) == seen[pre].end()))
+                {
                     successorMap[pre].push_back(str);
                     seen[pre].insert(str);
                 }
             }
         }
     }
-    int getChainLength(string word) {
-        if (resultMap.find(word) != resultMap.end()) {
-            return resultMap[word];
-        }
-        
-        int result = 0;
-        for(string successor: successorMap[word]) {
-            result = max(result, 1 + getChainLength(successor));
-        }
-        
-        resultMap[word] = result;
-        return result;
-    }
-    int longestStrChain(vector<string>& words) {
+    int longestStrChain(vector<string> &words)
+    {
+        sort(words.begin(), words.end(), compare);
         generateSuccessors(words);
-        
+        unordered_map<string, int> chainLen;
+
         int result = 0;
-        for(string word: words) {
-            int currentResult = 1+getChainLength(word);
-            // cout<<word<<": "<<currentResult<<endl;
-            result = max(result, currentResult);
+        for (int i = words.size() - 1; i >= 0; i--)
+        {
+            string word = words[i];
+
+            int maxLen = 1;
+            for (string successor : successorMap[word])
+            {
+                maxLen = max(maxLen, 1 + chainLen[successor]);
+            }
+
+            chainLen[word] = maxLen;
+            result = max(maxLen, result);
         }
-        
+
         return result;
     }
 };
